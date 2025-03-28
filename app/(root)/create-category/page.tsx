ef0f893507090060c2c048a142f7bb0b8c746ece
Namika <loud.music.n@gmail.com>
2025-03-29 05:43:23 +0900

    - commit

--- app/(root)/create-category/page.tsx
+++ app/(root)/create-category/page.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 const page = () => {
   return (
     <main className="px-24 mt-24">
-      Create Category
-      Put form
+      <p>Create Category</p>
+      <p>Form</p>
+      <p>: Category Name(input)</p>
+      <p>: Pin(check box, max total pinned category is 5 otherwise hidden)</p>
+      <p>Submit button(direct to /[id])</p>
     </main>
   )
 }
